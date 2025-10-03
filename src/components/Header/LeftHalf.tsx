2ee33233a4569326a713767877bcdea2e1830a61
shazaib991 <shazaib424647@gmail.com>
2025-10-03 21:36:56 +0500

    header left half structure

--- src/components/Header/LeftHalf.tsx
+++ src/components/Header/LeftHalf.tsx
@@ -1,10 +1,18 @@
 export const LeftHalf = () => {
   return (
-    <div className="flex">
+    <>
+    <div className="flex text-[12px] font-[Roboto_Mono] font-medium">
         <p>Hi!</p>
         <a href="#" className="underline text-blue-600">Sign in</a>
-        <p>or</p>
-        <a href="#">register</a>
+        <p className="mx-[2px]">or</p>
+        <a href="#" className="underline text-blue-600">register</a>
+        <div className="ml-[30px]">
+          <a href="#">Daily Deals</a>
+          <a href="#" className="ml-[20px]">Brand Outlet</a>
+          <a href="#" className="ml-[20px]">Gift Cards</a>
+          <a href="#" className="ml-[20px]">Help & Contact</a>
+        </div>
     </div>
+    </>
   )
 }
