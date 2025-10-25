8ed873ff8974d4158b20ceefdd39ab108dc3470b
shazaib991 <shazaib424647@gmail.com>
2025-10-25 13:31:33 +0500

    updated design of categoryPopover2

--- src/components/PopOvers/CategoryPopover2.tsx
+++ src/components/PopOvers/CategoryPopover2.tsx
@@ -1,4 +1,5 @@
 import {useSelector} from "react-redux";
+import {Link} from "react-router-dom";
 
 export const CategoryPopover2 = () => {
 	interface Category {
@@ -20,78 +21,201 @@ export const CategoryPopover2 = () => {
 
 	return (
 		<>
-			<div className={`${categoryName.innerText == "Furniture" ? "flex" : "hidden"}`}>
-				<div>
-					<p>Most popular categories</p>
+			<div className={`${categoryName.innerText == "Furniture" ? "flex" : "hidden"} w-full justify-between`}>
+				<div className="ml-[15px] mt-[15px] text-[13px] mr-[15px] w-[20%] flex flex-col">
+					<p className="font-bold mb-[5px]">Most popular categories</p>
 					<hr />
-					<p>Workshop Tools and Equipment</p>
-					<p>Patio, garden and outdoors</p>
-					<p>Home improvement</p>
-					<p>Kitchen, dining and bar</p>
-					<p>Lamps, lights and fans</p>
-					<p>Interior decoration</p>
-					<p>Home organization</p>
-					<p>Home appliances</p>
+					<div className="flex flex-col gap-2 mt-[10px]">
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Workshop Tools and Equipment
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Patio, garden and outdoors
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Home improvement
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Kitchen, dining and bar
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Lamps, lights and fans
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Interior decoration
+							</Link>
+						</div>
+					</div>
 				</div>
-				<div>
-					<p>More categories</p>
+
+				<div className="text-[13px] mt-[15px] mr-[15px] w-[20%] flex flex-col">
+					<p className="font-bold mb-[5px]">More categories</p>
 					<hr />
-					<p>Toys</p>
-					<p>Pets</p>
-					<p>Crafts</p>
-					<p>Art supplies</p>
-					<p>Musical instruments</p>
-					<p>Jewelry and beads</p>
-					<p>Deals</p>
-					<p>Sell on eBay</p>
+					<div className="flex flex-col gap-2 mt-[10px]">
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Toys
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Pets
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Crafts
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Art supplies
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Musical instruments
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Sell on eBay
+							</Link>
+						</div>
+					</div>
 				</div>
+
 				{productCategories.map((data, index) => {
 					return (
-						<div key={index} className={`${data.name == "Furniture" ? "block" : "hidden"}`}>
-							<div>
-								<p>{data.name}</p>
-								<p>Check the offers</p>
-								<button>Shop now</button>
+						<div
+							key={index}
+							className={`${
+								data.name == "Furniture" ? "flex" : "hidden"
+							} relative rounded-[20px] w-[60%] bg-[#1a1a17] my-[15px] mr-[15px]`}
+						>
+							<div className="absolute top-0 left-0 text-white mt-[35px] ml-[30px]">
+								<p className="text-[30px] text-[#e3e3e1] font-bold">{data.name}</p>
+								<p className="mt-[10px] text-[#e3e3e1] font-medium">Check the offers</p>
+								<button className="mt-[70px] w-[120px] bg-[#e3e3e1] text-[#1a1a18] rounded-full font-semibold text-[15px] py-[7px] px-[10px] cursor-pointer">
+									Shop now
+								</button>
 							</div>
-							<img src={data.image} alt="" />
+							<img
+								src={data.image}
+								className="w-[280px] h-full absolute top-0 right-[-1px] rounded-r-[20px]"
+								alt="furniture image"
+							/>
 						</div>
 					);
 				})}
 			</div>
-			<div className={`${categoryName.innerText == "Shoes" ? "flex" : "hidden"}`}>
-				<div>
-					<p>Most popular categories</p>
+
+			<div className={`${categoryName.innerText == "Shoes" ? "flex" : "hidden"} w-full justify-between`}>
+				<div className="ml-[15px] mt-[15px] text-[13px] mr-[15px] w-[20%] flex flex-col">
+					<p className="font-bold mb-[5px]">Most popular categories</p>
 					<hr />
-					<p>Cycling</p>
-					<p>Fitness, running and yoga</p>
-					<p>Fitness Tech</p>
-					<p>Fishing</p>
-					<p>Camping</p>
-					<p>Scooters</p>
-					<p>Team sports</p>
+					<div className="flex flex-col gap-2 mt-[10px]">
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Cycling
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Fitness, running and yoga
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Fitness Tech
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Fishing
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Camping
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Team sports
+							</Link>
+						</div>
+					</div>
 				</div>
-				<div>
-					<p>More categories</p>
+
+				<div className="text-[13px] mt-[15px] mr-[15px] w-[20%] flex flex-col">
+					<p className="font-bold mb-[5px]">More categories</p>
 					<hr />
-					<p>Watersports</p>
-					<p>Winter sports</p>
-					<p>Box and MMA</p>
-					<p>Swimming</p>
-					<p>GPS & Running</p>
-					<p>Watches</p>
-					<p>Garmin</p>
-					<p>Deals</p>
-					<p>Sell on eBay</p>
+					<div className="flex flex-col gap-2 mt-[10px]">
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Watersports
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Winter sports
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Box and MMA
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Swimming
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								GPS & Running
+							</Link>
+						</div>
+						<div className="w-fit">
+							<Link to="/" className="font-medium hover:underline">
+								Sell on eBay
+							</Link>
+						</div>
+					</div>
 				</div>
+
 				{productCategories.map((data, index) => {
 					return (
-						<div key={index} className={`${data.name == "Shoes" ? "block" : "hidden"}`}>
-							<div>
-								<p>{data.name}</p>
-								<p>Check the offers</p>
-								<button>Shop now</button>
+						<div
+							key={index}
+							className={`${
+								data.name == "Shoes" ? "flex" : "hidden"
+							} relative rounded-[20px] w-[60%] bg-[#a2b0bd] my-[15px] mr-[15px]`}
+						>
+							<div className="absolute top-0 left-0 text-white mt-[35px] ml-[30px]">
+								<p className="text-[30px] text-[#ffde66] font-bold">{data.name}</p>
+								<p className="mt-[10px] text-[#ffde66] font-medium">Check the offers</p>
+								<button className="mt-[50px] w-[120px] bg-[#ffde66] text-[#a2b0bd] rounded-full font-semibold text-[15px] py-[7px] px-[10px] cursor-pointer">
+									Shop now
+								</button>
 							</div>
-							<img src={data.image} alt="" />
+							<img
+								src={data.image}
+								className="w-[280px] h-full absolute top-0 right-[-1px] rounded-r-[20px]"
+								alt="shoes image"
+							/>
 						</div>
 					);
 				})}
