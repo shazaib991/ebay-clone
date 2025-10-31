8429ecf59fc2745985c3ff3a2043a265e7ebfc0e
shazaib991 <shazaib@gmail.com>
2025-10-31 21:31:03 +0500

    slideshow updates

--- src/components/SlideShow/SlideShow.tsx
+++ src/components/SlideShow/SlideShow.tsx
@@ -1,31 +1,117 @@
-import {Data} from "./SlideShowData";
+import {Data, Data2} from "./SlideShowData";
+import * as Icon from "react-bootstrap-icons";
 
 export const SlideShow = () => {
 	return (
-		<div>
-			{Data.map((item) => {
-				return (
-					<div className={`bg-${item.backgroundColor} flex items-center justify-between h-[400px] font-[arimo]`}>
-						<div>
-							<p className="text-[35px] font-bold">{item.title}</p>
-							<p>{item.description}</p>
-							<button>{item.buttonText}</button>
-						</div>
-						<div>
-							<div className="flex">
-								{item.products.map((item2) => {
-									return (
-										<div>
-											<img src={item2.image} className="h-[200px]" alt="" />
-											<p>{item2.title}</p>
-										</div>
-									);
-								})}
+		<div className="mt-[30px] relative h-[380px]">
+			<div className="flex absolute bottom-[15px] left-[50%] z-[5]">
+				{Data.map(() => {
+					return <div className="h-[7px] w-[7px] border rounded-full mr-[5px]"></div>;
+				})}
+				{Data2.map(() => {
+					return <div className="h-[7px] w-[7px] border rounded-full mr-[5px]"></div>;
+				})}
+			</div>
+			<div className="flex overflow-auto h-full">
+				{Data.map((item) => {
+					return (
+						<div
+							className={`bg-${item.backgroundColor} flex min-w-[100vw] items-center justify-between h-full font-[arimo] relative`}
+						>
+							<div className="ml-[80px] mb-[70px]">
+								<p className={`text-[45px] font-bold leading-12 mb-[10px] text-${item.textColor}`}>
+									{item.title}
+								</p>
+								<p className={`text-[15px] mb-[30px] text-${item.textColor} font-medium`}>{item.description}</p>
+								<button
+									className={`bg-${item.textColor} rounded-full py-[8px] font-bold text-${item.buttonTextColor} px-[20px] cursor-pointer`}
+								>
+									{item.buttonText}
+								</button>
+							</div>
+							<div>
+								<div className="flex mb-[35px]">
+									{item.products.map((item2) => {
+										return (
+											<>
+												<div className="flex flex-col items-center">
+													<img src={item2.image} className="h-[200px]" alt="" />
+													<div className="flex items-center">
+														<p className={`text-[18px] font-bold text-${item.textColor}`}>
+															{item2.title}
+														</p>
+														<Icon.ChevronRight
+															size={13}
+															className="ml-[10px]"
+															color={`${item.chevronColor}`}
+														></Icon.ChevronRight>
+													</div>
+												</div>
+											</>
+										);
+									})}
+								</div>
+							</div>
+							<div className="flex absolute bottom-[15px] right-[20px]">
+								<div className="bg-white rounded-full p-[8px] cursor-pointer">
+									<Icon.ChevronLeft></Icon.ChevronLeft>
+								</div>
+								<div className="bg-white rounded-full p-[8px] ml-[5px] cursor-pointer">
+									<Icon.ChevronRight></Icon.ChevronRight>
+								</div>
+								<div className="bg-white rounded-full p-[8px] ml-[5px] cursor-pointer">
+									<Icon.Pause></Icon.Pause>
+									<Icon.Play className="hidden"></Icon.Play>
+								</div>
 							</div>
 						</div>
-					</div>
-				);
-			})}
+					);
+				})}
+				{Data2.map((item) => {
+					return (
+						<div
+							className={`${item.backgroundColor} min-w-[100vw] flex items-center justify-between h-full font-[arimo] relative`}
+						>
+							<div className="ml-[80px] mb-[70px]">
+								<p className={`text-[45px] font-bold leading-12 mb-[10px] text-${item.textColor}`}>
+									{item.title}
+								</p>
+								<p className={`text-[15px] mb-[30px] text-${item.textColor} font-medium`}>{item.description}</p>
+								<button
+									className={`bg-${item.textColor} rounded-full py-[8px] font-bold ${item.buttonTextColor} px-[20px] cursor-pointer`}
+								>
+									{item.buttonText}
+								</button>
+							</div>
+							<div className="h-full">
+								<div className="h-full">
+									<img src={item.backgroundImage} className="h-full" alt="" />
+								</div>
+							</div>
+							<div className="flex absolute bottom-[15px] left-[50%]">
+								{Data.map(() => {
+									return <div className="h-[7px] w-[7px] border rounded-full mr-[5px]"></div>;
+								})}
+								{Data2.map(() => {
+									return <div className="h-[7px] w-[7px] border rounded-full mr-[5px]"></div>;
+								})}
+							</div>
+							<div className="flex absolute bottom-[15px] right-[20px]">
+								<div className="bg-white rounded-full p-[8px] cursor-pointer">
+									<Icon.ChevronLeft></Icon.ChevronLeft>
+								</div>
+								<div className="bg-white rounded-full p-[8px] ml-[5px] cursor-pointer">
+									<Icon.ChevronRight></Icon.ChevronRight>
+								</div>
+								<div className="bg-white rounded-full p-[8px] ml-[5px] cursor-pointer">
+									<Icon.Pause></Icon.Pause>
+									<Icon.Play className="hidden"></Icon.Play>
+								</div>
+							</div>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
--- src/components/SlideShow/SlideShowData.ts
+++ src/components/SlideShow/SlideShowData.ts
@@ -1,11 +1,25 @@
 import Image from "../../assets/images/pexels-olly-3757055_preview_rev_1.png"
 import Image1 from "../../assets/images/pexels-bidvine-517980-1249611_preview_rev_1.png"
 import Image2 from "../../assets/images/pexels-mohaned-tamzini-686673207-17938771_preview_rev_1.png"
+import Image3 from "../../assets/images/pexels-kampus-8931705_preview_rev_1.png"
 
 export const Data = [{
     backgroundColor: "blue-500",
     title: "All your faves are here",
+    textColor: "blue-950",
+    buttonTextColor: "blue-500",
+    chevronColor: "rgb(23,38,87)",
     description: "Refresh your space, elevate your style and power your work.",
     buttonText: "Do your thing",
     products: [{ image: Image, title: "Home and Garden" }, { image: Image2, title: "Fashion" }, { image: Image1, title: "Business and Industrial" }]
+}]
+
+export const Data2 = [{
+    backgroundColor: "bg-[#e7e7e7]",
+    backgroundImage: Image3,
+    textColor: "black",
+    buttonTextColor: "text-[#f7f7f7]",
+    title: "Buy more and save up to $120",
+    description: "Save on more of what you love, with easy international shipping.",
+    buttonText: "Get your coupon"
 }]
