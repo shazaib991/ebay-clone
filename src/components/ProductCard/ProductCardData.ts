2feb152b11feba0a962ebde855dfa9f9ae299b8b
shazaib991 <shazaib424647@gmail.com>
2025-11-13 13:22:04 +0500

    productCard in progress

---
+++ src/components/ProductCard/ProductCardData.ts
@@ -0,0 +1,8 @@
+import Image from '../../assets/images/productCardImages/pexels-tuurt-812264_preview_rev_1.png'
+import Image2 from '../../assets/images/productCardImages/pexels-olenkabohovyk-1772123_preview_rev_1.png'
+import Image3 from '../../assets/images/productCardImages/pexels-tuurt-818043_preview_rev_1.png'
+
+export const Data = [{
+    title: "The future in your hands",
+    products: [{ image: Image, title: "Laptop" }, { image: Image2, title: "Computer parts" }, { image: Image3, title: "Smartphones" }]
+}]
